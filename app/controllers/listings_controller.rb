4ee43e8e986cb95dbe83c43ae1b17551c0034531
Yi Suan Lee <l.yixuane@gmail.com>
2023-03-01 21:52:27 +0800

    Created listings controller and views

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,50 @@
+class ListingsController < ApplicationController
+  def index
+    @listings = Listing.all
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+
+    if @listing.save
+      redirect_to listings_path, notice: "Listing was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @listing = Listing.find(params[:id])
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+
+    if @listing.update(listing_params)
+      redirect_to listing_path(@listing), notice: "Listing was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+
+    redirect_to listings_path, notice: "Listing was successfully deleted."
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:name, :description, :price, :address, :user_id)
+  end
+end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,8 @@ class Listing < ApplicationRecord
   has_many :offers, dependent: :destroy
   has_many :reviews, dependent: :destroy
   belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :address, presence: true
 end
