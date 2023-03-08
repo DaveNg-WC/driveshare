747a2b5f55d7bf555422c7ea73d4696c70ef1e31
Xin Tang <tangxin1988@gmail.com>
2023-03-08 20:50:26 +0800

    update listing model and listings_new page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,5 +6,7 @@ class Listing < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true
   validates :address, presence: true
+  validates :transmission, presence: true
+  validates :category, presence: true
   has_one_attached :photo
 end
