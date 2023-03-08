9e00438a20e04e21e2edb162a6e3565d297e6085
Dave Ng <wcng77@hotmail.com>
2023-03-08 20:23:42 +0800

    add coordinates to listings migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_024145) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_122025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_024145) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "offers", force: :cascade do |t|
