8ce010edf2dae3baaafdb42268d03d639fd1a9d2
Dave Ng <wcng77@hotmail.com>
2023-03-08 20:37:36 +0800

    fix geocode validation error to listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ApplicationRecord
   geocoded_by :address # telling Geocoder to geocode address field
-  after_validation :geocode, if: :will_save_changes_to_address? # telling Geocoder to geocode only when changes made to address
+  after_validation :geocode, if: :will_save_change_to_address? # telling Geocoder to geocode only when changes made to address
   has_many :offers, dependent: :destroy
   has_many :reviews, dependent: :destroy
   belongs_to :user
